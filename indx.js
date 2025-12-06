efd193781819ba836668f545dfd1fed74116bc59
ZacharyChen <baicang007@outlook.com>
2025-12-06 21:06:35 +0800

    end

--- indx.js
+++ indx.js
@@ -8,9 +8,9 @@ window.onload = function load() {
 	if (lcStg == null || lcStg == "") {
 		lcStg = "我的主页 C ./resume.html";
 		lcStg += ",我的记录 S https://abc.baicang.top";
-		lcStg += ",百度 A https://www.baidu.com";
 		lcStg += ",翻译 F https://fanyi.baidu.com/?aldtype=85#zh/en/";
-		lcStg += ",哔哩 B https://www.bilibili.com,影视 Y https://www.gying.net/";
+		lcStg += ",百度 A https://www.baidu.com,iconfont I https://www.iconfont.cn";
+		lcStg += ",哔哩 B https://www.bilibili.com,影视 Y https://www.gying.net";
 		lcStg += ",Gitee G https://gitee.com,myAI D https://chat.deepseek.com";
 		lcStg += ",GitHub H https://github.com,Linux L https://www.linux.org";
 	}
